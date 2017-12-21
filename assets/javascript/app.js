22b9a75aafd7b57144f57428c054caff9fb41d4d
Quan Gao <quangao@Quans-MacBook-Pro.local>
2017-12-20 22:16:48 -0600

    make restart button functional and fix the unanswered counter

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
     var sets = [
         { 
-            Q: "What is the make, model and year of the car that Dean and Sam drive?",
+            Q: "1.  What is the make, model and year of the car that Dean and Sam drive?",
             A: [
                 {content: "1967 Chevy Impala", fact: "right" },
                 {content: "1977 Chevy Alpaca", fact: "wrong" },
@@ -12,7 +12,7 @@ $(document).ready(function(){
             ImgSrc: "assets/images/Q1.gif"
         },
         { 
-            Q: "What is Dean's favorite dessert?",
+            Q: "2.  What is Dean's favorite dessert?",
             A: [
                 {content: "Cake", fact: "wrong" },
                 {content: "Pie", fact: "right" },
@@ -22,7 +22,7 @@ $(document).ready(function(){
             ImgSrc: "assets/images/Q2.gif"
         },
         { 
-            Q: "What website is Sam's computer frozen on in the season 2 episode 'Tall Tales'?",
+            Q: "3.  What website is Sam's computer frozen on in the season 2 episode 'Tall Tales'?",
             A: [
                 {content: "www.topdeanbottomsamftw.com", fact: "wrong" },
                 {content: "www.finestfantasy.com", fact: "wrong" },
@@ -32,7 +32,7 @@ $(document).ready(function(){
             ImgSrc: "assets/images/Q3.jpg"
         },
         { 
-            Q: "What animal set Dean screaming when investigating an abandant mill in the season 4 episode 'Yellow Fever'?",
+            Q: "4.  What animal set Dean kicking & screaming when investigating an abandant mill in the season 4 episode 'Yellow Fever'?",
             A: [
                 {content: "A Cat", fact: "right" },
                 {content: "A Puppy", fact: "wrong" },
@@ -42,7 +42,7 @@ $(document).ready(function(){
             ImgSrc: "assets/images/Q4.gif"
         },
         { 
-            Q: "What's the name of the episode where Sam switched body with a teenage boy in season 5?",
+            Q: "5.  What's the name of the episode where Sam switched body with a teenage boy in season 5?",
             A: [
                 {content: "Free To Be You And Me", fact: "wrong" },
                 {content: "Sam, Interrupted", fact: "wrong" },
@@ -52,7 +52,7 @@ $(document).ready(function(){
             ImgSrc: "assets/images/Q5.gif"
         },  
         { 
-            Q: "Which superhero Dean likes to claim himself to be?",
+            Q: "6.  Which superhero Dean likes to claim himself to be?",
             A: [
                 {content: "Superman", fact: "wrong" },
                 {content: "Batman", fact: "right" },
@@ -62,7 +62,7 @@ $(document).ready(function(){
             ImgSrc: "assets/images/Q6.gif"
         }, 
         { 
-            Q: "Sam is the intended vessel of what Biblical figure?",
+            Q: "7.  Sam is the intended vessel of what Biblical figure?",
             A: [
                 {content: "Michael", fact: "wrong" },
                 {content: "Gabriel", fact: "wrong" },
@@ -72,7 +72,7 @@ $(document).ready(function(){
             ImgSrc: "assets/images/Q7.gif"
         },      
         { 
-            Q: "What does Dean call their car?",
+            Q: "8.  What does Dean call their car?",
             A: [
                 {content: "Bae", fact: "wrong" },
                 {content: "Babe", fact: "wrong" },
@@ -82,7 +82,7 @@ $(document).ready(function(){
             ImgSrc: "assets/images/Q8.gif"
         },      
         { 
-            Q: "Which town does Bobby Singer live nearby?",
+            Q: "9.  Which town does Bobby Singer live nearby?",
             A: [
                 {content: "Sioux Falls", fact: "right" },
                 {content: "Niagara Falls", fact: "wrong" },
@@ -92,7 +92,7 @@ $(document).ready(function(){
             ImgSrc: "assets/images/Q9.gif"
         },      
         { 
-            Q: "Who serves as the human vessel for Castiel and what is his profession?",
+            Q: "10. Who serves as the human vessel for Castiel and what is his profession?",
             A: [
                 {content: "Joseph Christian-Daddy", fact: "wrong" },
                 {content: "Jimmy Novak-Accountant", fact: "right" },
@@ -109,7 +109,7 @@ $(document).ready(function(){
     var wrongCounter = 0;
     var unansweredCounter = 0;
     var timer = {
-        time: 10,
+        time: 2,
         count: function(){
             if(timer.time <=0){
                 timer.stop();
@@ -121,7 +121,7 @@ $(document).ready(function(){
             }
         },
         reset: function(){
-            timer.time = 10;
+            timer.time = 2;
             $(".display").html(`<h2>Time Remaining: ${timer.time} seconds </h2>`);
             timerRunning = false;
         },
@@ -135,40 +135,44 @@ $(document).ready(function(){
             clearInterval(intervalID);
             timerRunning = false;
         }
-    }
+    };
     var displayPics = function(){
         var imagesource = $(".quiz").attr("data-source")
         $(".pics").attr("src", imagesource);    
-    }
+    };
     var displayCorrectChoice = function(){
         var correctAnswer = $("li[data-fact='right']");
         $(".correctA").text(`The correct answer is: ${correctAnswer.attr("data-content")}`) 
-    }
+    };
     var onRunOutTime = function(){
         $(".quiz").hide();
         $(".reveal").show();  
         $(".judge").text("You ran out of time!");
         displayCorrectChoice();     
         displayPics();
-        goToNextQuestion();
         unansweredCounter++;
-    }
+        if(n < 9){
+            goToNextQuestion();
+        } else{
+            onComplete();
+        };      
+    };
     var onChoose = function(){
         timer.stop();
         $(".quiz").hide();
         $(".reveal").show();
         displayPics();
-    }
+    };
     var onWrongAnswer = function(){
         $(".judge").text("You are so wrong!");
         displayCorrectChoice();
         wrongCounter++;  
-    }
+    };
     var onRightAnswer = function(){
         $(".correctA").empty();
         $(".judge").text("That's right!");
         correctCounter++;
-    }
+    };
     var askNewQuestion = function(i){
         var q = sets[i];
         $(".question").html(`<span>${q.Q}</span>`);
@@ -179,51 +183,54 @@ $(document).ready(function(){
             answerList.eq(i).attr("data-fact",q.A[i].fact);
             answerList.eq(i).attr("data-content",q.A[i].content);
         };
-    }
+    };
     var goToNextQuestion = function(){
-        n = n + 1;
-        if(n < 10){
-            setTimeout(function(){
-                $(".reveal").hide();
-                $(".quiz").show();
-                askNewQuestion(n);
-                timer.reset();
-                timer.start();
-            }, 4000);
-        } else{
-            onComplete();
-        }
+        n = n + 1;   
+        setTimeout(function(){
+            $(".reveal").hide();
+            $(".quiz").show();
+            askNewQuestion(n);
+            timer.reset();
+            timer.start();
+        }, 1000);
     };
 
     var restart = function(){
-        $(".summary").hide();
+        n = 0;
+        correctCounter = 0;
+        wrongCounter = 0;
+        unansweredCounter = 0;
+        $(".alldone").empty();
+        $(".score").empty();
         $(".brothers").show();
-        $("h1").addClass("start");
+        $("h1").attr("class","start");
         $("h1").text("Start Trivia Game");
     };
-
     var onComplete = function(){
         $(".reveal").hide();
         $(".summary").show();
         $(".alldone").html("<h2>All done, here's how you did!</h2>");
+        var restartButton = $("<li>");
+        restartButton.addClass("startOver");
+        restartButton.text("Start Over?");
         $(".score").append(`<li>Correct Answers: ${correctCounter}</li>`).
         append(`<li>Incorrect Answers: ${wrongCounter}</li>`).
-        append(`<li>Unanswered: ${unansweredCounter}</li>`);
-        var restartButton = $("<h2 class = 'startOver'>");
-        restartButton.text("start Over?");
-        $(".score").append(restartButton);
-        $(".summary").on("click", ".startOver", restart);
-    }
-
+        append(`<li>Unanswered: ${unansweredCounter}</li>`).append(restartButton);
+        $(".startOver").on("click",function(){
+            console.log("startover licked");
+            restart();});
+    };
+    
     $(".playground").on("click", ".start",function(){  
         $(".brothers").hide();
         $("h1").removeClass("start");
         $("h1").text("Supernatural Trivia");
         timer.reset();
         timer.start();
-        askNewQuestion(n);    
+        askNewQuestion(n);
+        $(".quiz").show();    
     });
-
+    
     $(".options").on("click","li", function(){    
         if(timerRunning){
             onChoose();
@@ -231,20 +238,12 @@ $(document).ready(function(){
                 onRightAnswer();
             } else {
                 onWrongAnswer();
-            } 
+            };
+        };
+        if(n < 9){
             goToNextQuestion();
-        } 
+        } else{
+            onComplete();
+        };       
     })
- 
-
-
-    
- 
-    
-
-
-
-
-
-
 });
